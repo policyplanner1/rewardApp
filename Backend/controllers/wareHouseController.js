dde50d191b69475b509330bdc29236edbb0f60d6
devashish <it@policyplanner.com>
2025-12-12 18:33:00 +0530

    get stock record

--- Backend/controllers/wareHouseController.js
+++ Backend/controllers/wareHouseController.js
@@ -14,7 +14,14 @@ class wareHouseController {
   async stockIn(req, res) {
     try {
       const wareHouseManagerId = req.user.user_id;
-      
+
+      if (!wareHouseManagerId) {
+        return res.status(400).json({
+          success: false,
+          message: "wareHouseManagerId is required",
+        });
+      }
+
       const {
         product_id,
         total_quantity,
@@ -45,10 +52,11 @@ class wareHouseController {
       // Insert
       await db.query(
         `INSERT INTO stock_in_entries
-            (grn, product_id, total_quantity, passed_quantity, failed_quantity, stock_in_date, location, expiry_date)
-            VALUES (?, ?, ?, ?, ?, ?, ?, ?)`,
+            (grn,warehousemanager_id, product_id, total_quantity, passed_quantity, failed_quantity, stock_in_date, location, expiry_date)
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)`,
         [
           grnNumber,
+          wareHouseManagerId,
           product_id,
           total_quantity,
           passed_quantity,
@@ -66,6 +74,49 @@ class wareHouseController {
       });
     } catch (err) {
       console.error("stock In Error:", err);
+      return res.status(500).json({ success: false, message: err.message });
+    }
+  }
+
+  async getDetails(req, res) {
+    try {
+      const status = req.query.status || "Pending";
+
+      const [rows] = await db.query(
+        `SELECT 
+          s.id,
+          s.warehousemanager_id,
+          s.grn,
+          s.total_quantity,
+          s.passed_quantity,
+          s.failed_quantity,
+          s.stock_in_date,
+          s.location,
+          s.expiry_date,
+          s.status,
+          p.product_name AS productName,
+          p.sku,
+          COALESCE(c.category_name, p.custom_category) AS categoryName,
+          v.full_name AS vendorName
+
+          FROM stock_in_entries s
+          JOIN products p ON s.product_id = p.product_id
+
+          LEFT JOIN categories c ON p.category_id = c.category_id
+          LEFT JOIN sub_categories sc ON p.subcategory_id = sc.subcategory_id
+          LEFT JOIN sub_sub_categories ssc ON p.sub_subcategory_id = ssc.sub_subcategory_id
+
+          JOIN vendors v ON p.vendor_id = v.vendor_id
+          JOIN users u ON s.warehousemanager_id = u.user_id
+
+          WHERE s.status = ? ;
+        `,
+        [status]
+      );
+
+      res.json({ success: true, data: rows });
+    } catch (err) {
+      console.error("Fetching stock record Error:", err);
       return res.status(500).json({ success: false, message: err.message });
     }
   }
--- Backend/routes/warehouseRoutes.js
+++ Backend/routes/warehouseRoutes.js
@@ -6,4 +6,7 @@ const wareHouseController = require("../controllers/wareHouseController");
 // create new entry
 router.post("/stock-in",authenticateToken,authorizeRoles('warehouse_manager'),wareHouseController.stockIn);
 
+// get the record data 
+router.get('stock-in',authenticateToken,authorizeRoles('warehouse_manager'),wareHouseController.getDetails)
+
 module.exports = router;
