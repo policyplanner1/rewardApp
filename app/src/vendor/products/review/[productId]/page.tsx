834c1323f65a63b2ae72012a445025eed7e97fec
devashish <it@policyplanner.com>
2025-12-16 12:50:41 +0530

    product view completed

--- app/src/vendor/products/review/[productId]/page.tsx
+++ app/src/vendor/products/review/[productId]/page.tsx
@@ -45,6 +45,7 @@ interface ProductView {
   subCategoryName?: string | null;
   subSubCategoryName?: string | null;
   gstIn?: string;
+  product_status?:string;
   variants?: VariantView[];
   productImages?: string[];
   requiredDocs?: Array<{
@@ -180,7 +181,7 @@ export default function ReviewProductPage({
         subSubCategoryName:
           raw.sub_subcategory_name ?? raw.custom_sub_subcategory ?? null,
 
-        gstIn: raw.gst ?? "",
+        product_status: raw.status ?? "",
         productImages: Array.isArray(raw.productImages)
           ? raw.productImages
           : raw.images ?? [],
@@ -266,12 +267,23 @@ export default function ReviewProductPage({
             </button>
 
             {/* Edit button - navigate to your edit route if exists */}
-            <Link
-              href={`/manager/products/edit/${product.productId}`}
+            {/* <Link
+              href={`/src/vendor/products/edit/${product.productId}`}
               className="flex items-center gap-2 px-4 py-2 text-sm font-medium border rounded-lg bg-white hover:bg-gray-50"
             >
               <FaEdit /> Edit
-            </Link>
+            </Link> */}
+            {!["approved", "rejected", "resubmission"].includes(
+              product.product_status
+            ) && (
+              <Link
+                href={`/src/vendor/products/edit/${product.productId}`}
+                target="_blank"
+                className="flex items-center gap-2 px-4 py-2 text-sm font-medium border rounded-lg bg-white hover:bg-gray-50"
+              >
+                <FaEdit />
+              </Link>
+            )}
           </div>
         </div>
 
@@ -579,9 +591,6 @@ export default function ReviewProductPage({
                     <div>
                       <div className="font-medium text-gray-800">
                         {doc.document_name}
-                      </div>
-                      <div className="text-xs text-gray-500">
-                        {doc.status == "1" ? "Required" : "Optional"}
                       </div>
                     </div>
 
