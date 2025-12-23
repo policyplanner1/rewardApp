c951e8df352e796b27a1eb25485092083d1594d9
devashish <it@policyplanner.com>
2025-12-23 18:08:33 +0530

    otp verification completed

--- Backend/utils/optGenerate.js
+++ Backend/utils/optGenerate.js
@@ -1,11 +1,11 @@
 const crypto = require("crypto");
 
 function generateOTP() {
-  return Math.floor(100000 + Math.random() * 900000).toString();
+  return String(Math.floor(100000 + Math.random() * 900000));
 }
 
 function hashOTP(otp) {
-  return crypto.createHash("sha256").update(otp).digest("hex");
+  return crypto.createHash("sha256").update(String(otp).trim()).digest("hex");
 }
 
-module.exports={generateOTP,hashOTP}
+module.exports = { generateOTP, hashOTP };
